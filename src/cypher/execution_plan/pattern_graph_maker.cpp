60649b18c05313d818d06dbe509324de86d1c16e
lipanpan03 <656461146@qq.com>
2024-06-26 16:49:41 +0800

    fix optional match by bug

--- src/cypher/execution_plan/pattern_graph_maker.cpp
+++ src/cypher/execution_plan/pattern_graph_maker.cpp
@@ -922,8 +922,15 @@ std::any PatternGraphMaker::visit(geax::frontend::CompositeQueryStatement* node)
 }
 
 std::any PatternGraphMaker::visit(geax::frontend::AmbientLinearQueryStatement* node) {
-    execution_plan_->SetReadOnly(true);
     auto& query_stmts = node->queryStatements();
+    int match_count = 0;
+    for (auto &stat : query_stmts) {
+        if (stat->type() == geax::frontend::AstNodeType::kMatchStatement) {
+            match_count++;
+        }
+    }
+    if (match_count > 1) CYPHER_TODO();
+    execution_plan_->SetReadOnly(true);
     for (auto query_stmt : query_stmts) {
         ACCEPT_AND_CHECK_WITH_ERROR_MSG(query_stmt);
     }
